f011cb68c413c4b69c98afdf010a78adf227eb7d
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-01-30 11:06:55 -0600

    UI Improved

--- src/safeNet/ATM.java
+++ src/safeNet/ATM.java
@@ -13,6 +13,7 @@ public class ATM {
 				for (int i = 0; i < 6; i++) {
 					bills[i] = 10;
 				}
+				System.out.println("Press: \n'W <$amount>' to withdraw\n'R' to restock\n'I <denomonations>' to see the bills\n'Q' to quit the program");
 				
 				command = input.nextLine();
 				
@@ -21,20 +22,22 @@ public class ATM {
 					if(splitString[0].equals("W")) {
 						splitString[1] = splitString[1].substring(1);
 						bills = controller.withdraw(splitString[1], bills);
-						System.out.println("$100 -" + bills[5]);
+						System.out.println();
+						System.out.print("$100 -" + bills[5] + "     ");
 						System.out.println("$50 -" + bills[4]);
-						System.out.println("$20 -" + bills[3]);
+						System.out.print("$20 -" + bills[3] + "     ");
 						System.out.println("$10 -" + bills[2]);
-						System.out.println("$5 -" + bills[1]);
+						System.out.print("$5 -" + bills[1] + "     ");
 						System.out.println("$1 -" + bills[0]);
 					}
 					else if(splitString[0].equals("R")) {
 						bills = controller.restock(bills);
-						System.out.println("$100 -" + bills[5]);
+						System.out.println();
+						System.out.print("$100 -" + bills[5] + "     ");
 						System.out.println("$50 -" + bills[4]);
-						System.out.println("$20 -" + bills[3]);
+						System.out.print("$20 -" + bills[3] + "     ");
 						System.out.println("$10 -" + bills[2]);
-						System.out.println("$5 -" + bills[1]);
+						System.out.print("$5 -" + bills[1] + "     ");
 						System.out.println("$1 -" + bills[0]);
 					}
 					else if(splitString[0].equals("I")) {
@@ -43,6 +46,7 @@ public class ATM {
 					else {
 						System.out.println("Failure:Invalid command");
 					}
+					System.out.println("\n\n\n\nPress:\n'W <$amount>' to withdraw\n'R' to restock\n'I <denomonations>' to see the bills\n'Q' to quit the program");
 					command = input.nextLine();
 				}
 				System.out.println("Program terminated.");	
